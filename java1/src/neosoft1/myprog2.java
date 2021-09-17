b0f3e29ae7b3b97d14f5ac49a6b01f7f9cbc5132
Vinay99kumar <vinay.kumar@neosoftmail.com>
2021-09-17 16:21:06 +0530

    add file by vinay

---
+++ java1/src/neosoft1/myprog2.java
@@ -0,0 +1,11 @@
+package neosoft1;
+
+public class myprog2 {
+
+	public static void main(String[] args) {
+		System.out.println("doond liya abhisek ko");
+		System.out.println("Ab marunga abhisek ko");
+
+	}
+
+}
