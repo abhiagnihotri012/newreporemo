133230b531f57516cec0a54907ef1299e07fbea2
PAVITHRA-UMESH <pavithrau2000@gmail.com>
2021-09-17 14:32:16 +0530

    initial commmit

---
+++ java1/src/neosoft1/MyProg1.java
@@ -0,0 +1,12 @@
+package neosoft1;
+
+public class MyProg1 {
+	public static void main(String args[]) {
+	System.out.println("How are you");
+	System.out.println("See the Errors!!!");
+	}
+}
+
+
+
+
