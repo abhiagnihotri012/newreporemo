08e36af19da0bda8a558e48ce251595654ce6b70
PAVITHRA-UMESH <pavithrau2000@gmail.com>
2021-09-20 14:17:15 +0530

    "I have pushed this file."

--- java1/src/neosoft1/MyProg1.java
+++ java1/src/neosoft1/MyProg1.java
@@ -1,7 +1,9 @@
 package neosoft1;
 
-public class MyProg1 {
-	public static void main(String args[]) {
+public class MyProg1 
+{
+	public static void main(String args[]) 
+	{
 	System.out.println("How are you");
 	System.out.println("See the Errors!!!");
 	}
